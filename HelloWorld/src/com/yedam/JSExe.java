66f2e85c2b9692acb483da7a5e42e1c4a830cb96
gywns <gywns8339@naver.com>
2025-05-14 17:50:32 +0900

    0514

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -1,8 +1,26 @@
 package com.yedam;
 
+import java.util.Scanner;
+
 public class JSExe {
+
 	public static void main(String[] args) {
-		test2();
+		int num1 = 30;
+		int num2 = 30;
+
+//		System.out.println(num1 == num2);
+
+		String str1 = new String("Hello"); // 원래 방법
+		String str2 = new String("Hello");
+
+//		System.out.println(str1);
+//		System.out.println(str2);
+
+//		System.out.println(str1.equals(str2));
+
+//		test2();
+		// test3();
+		test4();
 	} // main
 
 	public static void test() {
@@ -13,19 +31,55 @@ public class JSExe {
 				sum += i;
 			}
 		}
-		System.out.println(sum);
+		System.out.println("test: " + sum);
 	} // test
 
 	public static void test2() {
-		// 임의의 수를 생성 (1 ~ 100사이의 임의의 값)
-		// 1 <= x < 11
+		// 임의의 수를 생성 (30 ~ 100사이의 임의의 값)
 		int sum = 0;
-		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 100) + 1;
-			System.out.println(result);
+		for (int i = 1; i <= 7; i++) {
+			int result = (int) (Math.random() * 71) + 30;
 			sum += result;
 		}
-
-		System.out.println(sum);
+		double avg = sum / 7.0;
+		System.out.println("결과: " + sum);
+		System.out.println("결과: " + Math.round(avg * 100) / 100.0);
+		System.out.println("결과: " + String.format("%.3f", avg / 5));
 	} // test2
+
+	public static void test3() {
+		Scanner scn = new Scanner(System.in);
+		int sum = 0;
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("학생의 점수를 입력>>");
+			String value = scn.nextLine();
+			int score = Integer.parseInt(value);
+			sum += score;
+		}
+		double avg = sum / 3.0;
+		avg = Math.round(avg * 100) / 100.0;
+		System.out.println("합계: " + sum + ", 평균: " + avg);
+	} // test3
+
+	public static void test4() {
+		Scanner scn = new Scanner(System.in);
+		String name = "";
+		while (true) {
+			System.out.print("메세지를 입력하세요. 종료하려면 quit>> ");
+			String msg = scn.nextLine();
+			
+			if (name.equals("")) {
+				name += msg;
+			} else if (msg.equals("quit")) {
+				name += " 입니다.";
+				System.out.println("종료합니다.");
+				break;
+			} else {
+				name += ", " + msg;
+			}
+			
+			System.out.println("입력한 값은: " + msg);
+		}
+		System.out.println("친구목록은 " + name);
+	} // test4
 }
