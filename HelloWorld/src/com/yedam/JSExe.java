dccd58f3ee30ff75d88a9b18c429f8c22662ccd2
gywns <gywns8339@naver.com>
2025-05-14 12:44:13 +0900

    mm

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -2,16 +2,7 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
-		// 임의의 수를 생성 (1 ~ 100사이의 임의의 값)
-		// 1 <= x < 11
-		int sum = 0;
-		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 100) + 1;
-			System.out.println(result);
-			sum += result;
-		}
-		
-		System.out.println(sum);
+		test2();
 	} // main
 
 	public static void test() {
@@ -24,4 +15,17 @@ public class JSExe {
 		}
 		System.out.println(sum);
 	} // test
+
+	public static void test2() {
+		// 임의의 수를 생성 (1 ~ 100사이의 임의의 값)
+		// 1 <= x < 11
+		int sum = 0;
+		for (int i = 1; i <= 5; i++) {
+			int result = (int) (Math.random() * 100) + 1;
+			System.out.println(result);
+			sum += result;
+		}
+
+		System.out.println(sum);
+	} // test2
 }
